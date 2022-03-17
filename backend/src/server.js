f655a60e567b33b14b9918f4171246da343050c6
Aidan Daly <dalyaidan1@gmail.com>
2022-03-17 15:42:20 +0000

    remove test sanitization script

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,24 +1,9 @@
-// const browserObject = require('./scraper/browser')
-// const scraperController = require('./appController')
-// const driver = require('./database/neo4jDriver')
+const browserObject = require('./scraper/browser')
+const scraperController = require('./appController')
+const driver = require('./database/neo4jDriver')
 
-// //Start the browser and create a browser instance
-// let browserInstance = browserObject.startBrowser()
+//Start the browser and create a browser instance
+let browserInstance = browserObject.startBrowser()
 
-// // Pass the browser instance to the scraper controller
-// scraperController(browserInstance, driver)
-
-
-const sanitize = require('./sanitizer/pageSanitizer')
-const {deScapeContent} = require('./generator/generateHelpers')
-const fs = require('fs')
-
-const sanitizeThis = fs.readFileSync('public/html/unsane.html', 'utf8').replace(/\\n/g,'')
-
-let sani = sanitize(sanitizeThis, "https://quotes.toscrape.com")
-
-fs.writeFileSync('public/html/sane.html', sani) 
-
-let descsani = deScapeContent(sani)
-
-fs.writeFileSync('public/html/saneScape.html', descsani) 
+// Pass the browser instance to the scraper controller
+scraperController(browserInstance, driver)
