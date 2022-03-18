00a87b7fc4e8390fbd624b54bddaf3cdfd76b182
Aidan Daly <dalyaidan1@gmail.com>
2022-03-18 11:10:42 +0000

    missing staging file

---
+++ client/src/fetch.js
@@ -0,0 +1,40 @@
+export const getData = async (endpoint) => {
+    await fetch(`${process.env.REACT_APP_BACKEND_URL}/api/${endpoint}`, {
+        method:'GET',
+        mode: 'cors',
+        cache: 'no-cache',
+        credentials: 'same-origin',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        redirect: 'follow',
+        referrerPolicy: 'no-referrer',
+      })
+      .then((res) => {
+        return res.json()
+      })
+      .catch((err) => {
+        return err
+      })
+}
+
+export const postData = async (endpoint, data) => {
+    await fetch(`${process.env.REACT_APP_BACKEND_URL}/api/${endpoint}`, {
+        method:'POST',
+        mode: 'cors',
+        cache: 'no-cache',
+        credentials: 'same-origin',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        redirect: 'follow',
+        referrerPolicy: 'no-referrer',
+        body: JSON.stringify({data})
+      })
+      .then((res) => {
+        return res.json()
+      })
+      .catch((err) => {
+        return err
+      })
+}
